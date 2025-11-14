82f8f5a35af810e64828db8b9b5330f58f28503e
SurabB <mbhattarai243@gmail.com>
2025-11-14 19:07:57 +0545

    method now returns values instead of printing in console

--- src/main/java/com/expr/EulerRk2Converter.java
+++ src/main/java/com/expr/EulerRk2Converter.java
@@ -1,33 +1,59 @@
 package com.expr;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.function.BiFunction;
-class EulerRk2Converter {
-    public static void main(String[] args) {
-        //  new EulerRk2Converter().rk2(1,2,0.5,(x,y)->(2*y)/x,2);
-        // new EulerRk2Converter().euler(0,1,0.1,(x,y)->2*x*y,3);
-        new EulerRk2Converter().rk2(1,2,0.25,(x, y)->y+Math.sin(x),2);
-        //x0,y0,h,dy/dx,no.of itr
-    }
+public class EulerRk2Converter {
 
-    public void rk2(double xnow,double ynow,double h,BiFunction<Double,Double,Double>func,int itr){
+//    public static void main(String[] args) {
+//        //   EulerRk2Converter.rk2(1,2,0.5,(x,y)->(2*y)/x,2);
+//        //  EulerRk2Converter.euler(0,1,0.1,(x,y)->2*x*y,3);
+//        //x0,y0,h,dy/dx,no.of itr
+//        HashMap<Integer, Values> values = EulerRk2Converter.rk2(1, 2, 0.25, (x, y) -> y + Math.sin(x), 2);
+//        for(Map.Entry<Integer, Values> entry:values.entrySet()){
+//            int key=entry.getKey();
+//            double x=entry.getValue().getXnow();
+//            double y=entry.getValue().getYnow();
+//            System.out.println("x%d = %.4f , y%d = %.4f".formatted(key,x,key,y));
+//
+//        }
+//
+//    }
 
-        for(int i=0;i<itr;i++){
+    public static  HashMap<Integer, Values> rk2(double xnow,double ynow,double h,BiFunction<Double,Double,Double>func,int itr){
+     HashMap<Integer,Values> values=new LinkedHashMap<>(itr);
+        for(int i=0;i<=itr;i++){
             double ynext=ynow+(h/2)*(func.apply(xnow,ynow)+func.apply(xnow+h,ynow+h*func.apply(xnow,ynow)));
+            values.put(i,new Values(xnow,ynow));
             ynow=ynext;
             xnow=xnow+h;
-            System.out.println("x"+(i+1)+"= "+xnow+ ", y"+(i+1)+ "= "+ynow);
-
         }
+        return values;
     }
-    public void euler(double xnow,double ynow,double h,BiFunction<Double,Double,Double>func,int itr){
-
+    public  static HashMap<Integer, Values> euler(double xnow, double ynow, double h, BiFunction<Double,Double,Double>func, int itr){
+        HashMap<Integer,Values> values=new LinkedHashMap<>(itr);
         for(int i=0;i<itr;i++){
             double ynext=ynow+h*func.apply(xnow,ynow);
+            values.put(i,new Values(xnow,ynow));
             ynow=ynext;
             xnow=xnow+h;
-            System.out.println("x"+(i+1)+"= "+xnow+ ", y"+(i+1)+ "= "+ynow);
+        }
+           return values;
+    }
+    public static class Values{
+        private final double xnow;
+       private final double ynow;
+        Values(double xnow, double ynow){
+            this.xnow=xnow;
+            this.ynow=ynow;
 
         }
-
+        public double getXnow(){
+            return xnow;
+        }
+        public double getYnow(){
+            return ynow;
+        }
     }
+
 }
