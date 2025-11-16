78d238670865c9f87e7170641fd34a5a92ae1e27
SurabB <mbhattarai243@gmail.com>
2025-11-16 19:11:07 +0545

    corrected hashmap initial size from itr to itr+1

--- src/main/java/com/expr/EulerRk2Converter.java
+++ src/main/java/com/expr/EulerRk2Converter.java
@@ -12,25 +12,25 @@ interface TriFunction<T>{
 
 public class EulerRk2Converter {
 
-    public static void main(String[] args) {
-        //   EulerRk2Converter.rk2(1,2,0.5,(x,y)->(2*y)/x,2);
-
-        //HashMap<Integer, Values> values = EulerRk2Converter.firstOrderDiffUsingEuler(0, 1, 0.1, (x, y) -> 2 * x * y, 3);
-        //x0,y0,h,dy/dx,no.of itr
-
-        //HashMap<Integer, Values> values = EulerRk2Converter.rk2(1, 2, 0.25, (x, y) -> y + Math.sin(x), 2);
-
-        HashMap<Integer, Values> values = EulerRk2Converter.secondOrderDiffUsingEuler(0, 0, 1, 0.2, (x, y, z) -> 6 + 3 * y - 2 * z, 2);
-        //x0,y0,z0,h,dz/dx,no.of itr
-        values.forEach((key,value)->{
-            System.out.println("itr: %d , values = %s".formatted(key,value.getValues()));
-        });
-
-    }
+//    public static void main(String[] args) {
+//        //   EulerRk2Converter.rk2(1,2,0.5,(x,y)->(2*y)/x,2);
+//
+//        //HashMap<Integer, Values> values = EulerRk2Converter.firstOrderDiffUsingEuler(0, 1, 0.1, (x, y) -> 2 * x * y, 3);
+//        //x0,y0,h,dy/dx,no.of itr
+//
+//        //HashMap<Integer, Values> values = EulerRk2Converter.rk2(1, 2, 0.25, (x, y) -> y + Math.sin(x), 2);
+//
+//        HashMap<Integer, Values> values = EulerRk2Converter.secondOrderDiffUsingEuler(0, 0, 1, 0.2, (x, y, z) -> 6 + 3 * y - 2 * z, 2);
+//        //x0,y0,z0,h,dz/dx,no.of itr
+//        values.forEach((key,value)->{
+//            System.out.println("itr: %d , values = %s".formatted(key,value.getValues()));
+//        });
+//
+//    }
 
     public static  HashMap<Integer, Values> rk2(double xnow,double ynow,double h,BiFunction<Double,Double,Double>func,int itr){
         //stores values  x and y for each iteration including initial ones
-     HashMap<Integer,Values> values=new LinkedHashMap<>(itr);
+     HashMap<Integer,Values> values=new LinkedHashMap<>(itr+1);
         for(int i=0;i<=itr;i++){
             double ynext=ynow+(h/2)*(func.apply(xnow,ynow)+func.apply(xnow+h,ynow+h*func.apply(xnow,ynow)));
             values.put(i,new Values(List.of(xnow,ynow)));
@@ -42,7 +42,7 @@ public class EulerRk2Converter {
     }
     public  static HashMap<Integer, Values> firstOrderDiffUsingEuler(double xnow, double ynow, double h, BiFunction<Double,Double,Double>func, int itr){
         //stores values  x and y for each iteration including initial ones
-        HashMap<Integer,Values> values=new LinkedHashMap<>(itr);
+        HashMap<Integer,Values> values=new LinkedHashMap<>(itr+1);
         for(int i=0;i<=itr;i++){
             double ynext=ynow+h*func.apply(xnow,ynow);
             values.put(i,new Values(List.of(xnow,ynow)));
@@ -54,7 +54,7 @@ public class EulerRk2Converter {
     }
     public static  HashMap<Integer, Values> secondOrderDiffUsingEuler(double xnow,double ynow,double znow,double h,TriFunction<Double> func,int itr){
         //stores values  x and y for each iteration including initial ones
-        HashMap<Integer,Values> values=new LinkedHashMap<>(itr);
+        HashMap<Integer,Values> values=new LinkedHashMap<>(itr+1);
         for(int i=0;i<=itr;i++){
            double znext=znow+h*func.apply(xnow,ynow,znow);
             double ynext=ynow+h*znow;
