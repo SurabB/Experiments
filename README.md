c55f07a85da8d0373ea6d8988a344ce79ebf862c
SurabB <mbhattarai243@gmail.com>
2025-11-17 19:20:55 +0545

    added heun's second order diffrn solving method

--- README.md
+++ README.md
@@ -3,14 +3,22 @@
 <h1> Experimental</h1>
 <h2>Euler and Rk2 converter-->src/main/java/com/expr/EulerRk2Converter.java</h2>
 <p> Takes initial values x<sub>0</sub>, y<sub>0</sub>, h, func(x,y) and no of itr and perform necessary opr</p>
-<p>method firstOrderDiffUsingEuler()-> follows formula: y<sub>n+1</sub> = y<sub>n</sub>+h * F[x<sub>n</sub>, y<sub>n</sub>], and x<sub>n+1</sub>= x<sub>n</sub> + h</p>
-<p>method secondOrderDiffUsingEuler()-> follows formula: y<sub>n+1</sub> = y<sub>n</sub>+h * F[x<sub>n</sub>, y<sub>n</sub>,z<sub>n</sub>], where[x,y,z] is assumed to be dy/dx=z and x<sub>n+1</sub>= x<sub>n</sub> + h</p>
+<p>method firstOrderDiffUsingEuler()-> follows Euler formula: y<sub>n+1</sub> = y<sub>n</sub>+h * F[x<sub>n</sub>, y<sub>n</sub>], and x<sub>n+1</sub>= x<sub>n</sub> + h</p>
+<p>method secondOrderDiffUsingEuler()-> follows Euler formula: y<sub>n+1</sub> = y<sub>n</sub>+h * F[x<sub>n</sub>, y<sub>n</sub>,z<sub>n</sub>], where[x,y,z] is assumed to be dy/dx=z and x<sub>n+1</sub>= x<sub>n</sub> + h</p>
 <p>z<sub>n+1</sub>=z<sub>n</sub>+h*g[x<sub>n</sub>,y<sub>n</sub>,z<sub>n</sub>] ,where g[x,y,z] is assumed to be dz/dx
 <br>
 and x<sub>n+1</sub>= x<sub>n</sub> + h</p>
 
-<p>method rk2()-> follows formula: y<sub>n+1</sub> = y<sub>n</sub>+(h/2) * [F(x<sub>n</sub>, y<sub>n</sub>) + F(x<sub>n+1</sub>,y<sub>n+1</sub> <sup>euler</sup>)]  and x<sub>n+1</sub>=x<sub>n</sub> + h <br> and y<sub>n+1</sub> <sup>euler</sup>
+<p>method firstOrderDiffUsingRk2()-> follows Heun's formula: y<sub>n+1</sub> = y<sub>n</sub>+(h/2) * [F(x<sub>n</sub>, y<sub>n</sub>) + F(x<sub>n+1</sub>,y<sub>n+1</sub> <sup>euler</sup>)]  and x<sub>n+1</sub>=x<sub>n</sub> + h <br> and y<sub>n+1</sub> <sup>euler</sup>
   =y<sub>n</sub> + h * F[x<sub>n</sub>, y<sub>n</sub>]</p>
+
+<p>method secondOrderDiffUsingRk2()-> follows Heun's formula: y<sub>n+1</sub> = y<sub>n</sub>+(h/2) * [F(x<sub>n</sub>, y<sub>n</sub> , z<sub>n</sub>) + F(x<sub>n+1</sub>,y<sub>n+1</sub> <sup>euler</sup>), z<sub>n+1</sub> <sup>euler</sup>)]  and x<sub>n+1</sub>=x<sub>n</sub> + h <br> and
+y<sub>n+1</sub> <sup>euler</sup> =y<sub>n</sub> + h * F[x<sub>n</sub>, y<sub>n</sub> ,z<sub>n</sub>] where F[x<sub>n</sub>, y<sub>n</sub> ,z<sub>n</sub>]=z<sub>n</sub>
+<br>
+also,
+z<sub>n+1</sub> <sup>euler</sup> =z<sub>n</sub> + h * g[x<sub>n</sub>, y<sub>n</sub> ,z<sub>n</sub>] where g[x<sub>n</sub>, y<sub>n</sub> ,z<sub>n</sub>]=dz/dx
+
+</p>
 
 <h2>LibraryManagementSystem->src/main/java/com/expr/LibraryManagementSys.java </h2>
 <p> This is console based library Management System application which has basic modules:</p>
@@ -21,6 +29,6 @@ and x<sub>n+1</sub>= x<sub>n</sub> + h</p>
 
 <b>Limitations</b>
 <p>1 Console based app. </p>
-<p>2 Verification mechnaism not included for business</p>
+<p>2 Verification mechanism not included for business</p>
 <p>3 No payment mechanism</p>
 <p>4 No history maintained</p>
--- src/main/java/com/expr/EulerRk2Converter.java
+++ src/main/java/com/expr/EulerRk2Converter.java
@@ -13,68 +13,105 @@ interface TriFunction<T>{
 public class EulerRk2Converter {
 
 //    public static void main(String[] args) {
-//        //   EulerRk2Converter.rk2(1,2,0.5,(x,y)->(2*y)/x,2);
+//           EulerRk2Converter.firstOrderDiffUsingRk2(1,2,0.5,(x,y)->(2*y)/x,2);
 //
-//        //HashMap<Integer, Values> values = EulerRk2Converter.firstOrderDiffUsingEuler(0, 1, 0.1, (x, y) -> 2 * x * y, 3);
+//       // HashMap<Integer, List<Double>> values = EulerRk2Converter.firstOrderDiffUsingEuler(0, 1, 0.1, (x, y) -> 2 * x * y, 3);
 //        //x0,y0,h,dy/dx,no.of itr
 //
-//        //HashMap<Integer, Values> values = EulerRk2Converter.rk2(1, 2, 0.25, (x, y) -> y + Math.sin(x), 2);
-//
-//        HashMap<Integer, Values> values = EulerRk2Converter.secondOrderDiffUsingEuler(0, 0, 1, 0.2, (x, y, z) -> 6 + 3 * y - 2 * z, 2);
+//      //  HashMap<Integer, List<Double>> values = EulerRk2Converter.firstOrderDiffUsingRk2(1, 2, 0.25, (x, y) -> y + Math.sin(x), 2);
+//         //x0,y0,h,dy/dx,no.of itr
+
+//       // HashMap<Integer, List<Double>> values = EulerRk2Converter.secondOrderDiffUsingEuler(0, 0, 1, 0.2, (x, y, z) -> 6 + 3 * y - 2 * z, 2);
 //        //x0,y0,z0,h,dz/dx,no.of itr
+//
+//        HashMap<Integer, List<Double>> values = EulerRk2Converter.secondOrderDiffUsingRk2(0, 0, 0, 0.25, (x, y, z) -> 5 - 2 * z + 4 * y, 2);
+//          //x0,y0,z0,h,dz/dx,no.of itr
 //        values.forEach((key,value)->{
-//            System.out.println("itr: %d , values = %s".formatted(key,value.getValues()));
+//            System.out.println("itr: %d , values = %s".formatted(key,value));
 //        });
 //
 //    }
 
-    public static  HashMap<Integer, Values> rk2(double xnow,double ynow,double h,BiFunction<Double,Double,Double>func,int itr){
+    public static  HashMap<Integer, List<Double>> firstOrderDiffUsingRk2(double xnow,double ynow,double h,BiFunction<Double,Double,Double>func,int itr){
         //stores values  x and y for each iteration including initial ones
-     HashMap<Integer,Values> values=new LinkedHashMap<>(itr+1);
+     HashMap<Integer,List<Double>> values=new LinkedHashMap<>(itr+1);
         for(int i=0;i<=itr;i++){
+            //follows Heun's y n+1 formula
             double ynext=ynow+(h/2)*(func.apply(xnow,ynow)+func.apply(xnow+h,ynow+h*func.apply(xnow,ynow)));
-            values.put(i,new Values(List.of(xnow,ynow)));
+
+            //stores current values x,y
+            values.put(i,List.of(xnow,ynow));
+
+            //update the values
             ynow=ynext;
             xnow=xnow+h;
         }
         //returns values x and y of each iteration including the initial values.
         return values;
     }
-    public  static HashMap<Integer, Values> firstOrderDiffUsingEuler(double xnow, double ynow, double h, BiFunction<Double,Double,Double>func, int itr){
-        //stores values  x and y for each iteration including initial ones
-        HashMap<Integer,Values> values=new LinkedHashMap<>(itr+1);
+    public static  HashMap<Integer,List<Double>> secondOrderDiffUsingRk2(double xnow,double ynow,double znow,double h,TriFunction<Double>func,int itr){
+        //stores values  x,y,z for each iteration including initial ones
+        HashMap<Integer,List<Double>> values=new LinkedHashMap<>(itr+1);
         for(int i=0;i<=itr;i++){
+            //follows Heun's z n+1 euler formula
+            double znowEuler=znow+h*func.apply(xnow,ynow,znow);
+
+            //follows Heun's y n+1 euler formula
+            double ynowEuler=ynow+h*znow;
+
+            //follows Heun's y n+1 formula
+            double ynext=ynow+(h/2)*(znow+znowEuler);
+
+            //follows Heun's  z n+1 formula
+            double znext=znow+(h/2)*(func.apply(xnow,ynow,znow)+func.apply(xnow,ynowEuler,znowEuler));
+
+            //stores current x,y,z
+            values.put(i,List.of(xnow,ynow,znow));
+
+            //update the values
+            ynow=ynext;
+            xnow=xnow+h;
+            znow=znext;
+
+        }
+        return values;
+    }
+    public  static HashMap<Integer, List<Double>> firstOrderDiffUsingEuler(double xnow, double ynow, double h, BiFunction<Double,Double,Double>func, int itr){
+        //stores values  x and y for each iteration including initial ones
+        HashMap<Integer,List<Double>> values=new LinkedHashMap<>(itr+1);
+        for(int i=0;i<=itr;i++){
+            //follows euler y n+1 formula
             double ynext=ynow+h*func.apply(xnow,ynow);
-            values.put(i,new Values(List.of(xnow,ynow)));
+
+            //stores current value of x and y
+            values.put(i,List.of(xnow,ynow));
+
+            //update the values
             ynow=ynext;
             xnow=xnow+h;
         }
-        //returns values x and y of each iteration including the initial values.
         return values;
     }
-    public static  HashMap<Integer, Values> secondOrderDiffUsingEuler(double xnow,double ynow,double znow,double h,TriFunction<Double> func,int itr){
-        //stores values  x and y for each iteration including initial ones
-        HashMap<Integer,Values> values=new LinkedHashMap<>(itr+1);
+    public static  HashMap<Integer, List<Double>> secondOrderDiffUsingEuler(double xnow,double ynow,double znow,double h,TriFunction<Double> func,int itr){
+        //stores values  x,y,z  for each iteration including initial ones
+        HashMap<Integer,List<Double>> values=new LinkedHashMap<>(itr+1);
         for(int i=0;i<=itr;i++){
+            //follows euler z n+1 formula
            double znext=znow+h*func.apply(xnow,ynow,znow);
+
+           //follows euler y n+1 formula
             double ynext=ynow+h*znow;
-            values.put(i,new Values(List.of(xnow,ynow,znow)));
+
+            //stores current value of x,y,z
+            values.put(i,List.of(xnow,ynow,znow));
+
+            //update the values
             ynow=ynext;
             xnow=xnow+h;
             znow=znext;
         }
-        //returns values x and y of each iteration including the initial values.
         return values;
     }
-    public static class Values{
-      private final List<Double> values;
-        Values(List<Double> values){
-        this.values=values;
 
-        }
-  public List<Double> getValues(){
-            return values;
-  }
-    }
 
 }
