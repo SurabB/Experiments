29d0b0fbca9af8be723e4c75cf06e687eced3453
SurabB <mbhattarai243@gmail.com>
2025-11-18 15:32:00 +0545

    added rk4 solver for second order diffrn eqn solving

--- src/main/java/com/expr/RungeKuttaSolver.java
+++ src/main/java/com/expr/RungeKuttaSolver.java
@@ -3,34 +3,36 @@ package com.expr;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.function.BiFunction;
 
 interface TriFunction<T>{
     T apply(T a, T b, T c);
 }
 
-public class EulerRk2Converter {
+public class RungeKuttaSolver {
 
-//    public static void main(String[] args) {
-//           EulerRk2Converter.firstOrderDiffUsingRk2(1,2,0.5,(x,y)->(2*y)/x,2);
-//
-//       // HashMap<Integer, List<Double>> values = EulerRk2Converter.firstOrderDiffUsingEuler(0, 1, 0.1, (x, y) -> 2 * x * y, 3);
-//        //x0,y0,h,dy/dx,no.of itr
-//
-//      //  HashMap<Integer, List<Double>> values = EulerRk2Converter.firstOrderDiffUsingRk2(1, 2, 0.25, (x, y) -> y + Math.sin(x), 2);
-//         //x0,y0,h,dy/dx,no.of itr
+    public static void main(String[] args) {
+           //EulerRk2Converter.firstOrderDiffUsingRk2(1,2,0.5,(x,y)->(2*y)/x,2);
 
-//       // HashMap<Integer, List<Double>> values = EulerRk2Converter.secondOrderDiffUsingEuler(0, 0, 1, 0.2, (x, y, z) -> 6 + 3 * y - 2 * z, 2);
-//        //x0,y0,z0,h,dz/dx,no.of itr
-//
-//        HashMap<Integer, List<Double>> values = EulerRk2Converter.secondOrderDiffUsingRk2(0, 0, 0, 0.25, (x, y, z) -> 5 - 2 * z + 4 * y, 2);
-//          //x0,y0,z0,h,dz/dx,no.of itr
-//        values.forEach((key,value)->{
-//            System.out.println("itr: %d , values = %s".formatted(key,value));
-//        });
-//
-//    }
+       // HashMap<Integer, List<Double>> values = EulerRk2Converter.firstOrderDiffUsingEuler(0, 1, 0.1, (x, y) -> 2 * x * y, 3);
+        //x0,y0,h,dy/dx,no.of itr
+
+      //  HashMap<Integer, List<Double>> values = EulerRk2Converter.firstOrderDiffUsingRk2(1, 2, 0.25, (x, y) -> y + Math.sin(x), 2);
+         //x0,y0,h,dy/dx,no.of itr
+
+       // HashMap<Integer, List<Double>> values = EulerRk2Converter.secondOrderDiffUsingEuler(0, 0, 1, 0.2, (x, y, z) -> 6 + 3 * y - 2 * z, 2);
+        //x0,y0,z0,h,dz/dx,no.of itr
+
+        //HashMap<Integer, List<Double>> values = EulerRk2Converter.secondOrderDiffUsingRk2(0, 0, 0, 0.25, (x, y, z) -> 5 - 2 * z + 4 * y, 2);
+          //x0,y0,z0,h,dz/dx,no.of itr
+
+        HashMap<Integer, List<Double>> values = RungeKuttaSolver.secondOrderDiffUsingRk4(0, 0, 0, 0.25, (x, y, z )-> 5 + 4 * y - 2 * z, 2);
+        //x0,y0,z0,h,dz/dx,no.of itr
+        values.forEach((key,value)->{
+            System.out.println("itr: %d , values = %s".formatted(key,value));
+        });
+
+    }
 
     public static  HashMap<Integer, List<Double>> firstOrderDiffUsingRk2(double xnow,double ynow,double h,BiFunction<Double,Double,Double>func,int itr){
         //stores values  x and y for each iteration including initial ones
@@ -109,6 +111,37 @@ public class EulerRk2Converter {
             ynow=ynext;
             xnow=xnow+h;
             znow=znext;
+        }
+        return values;
+    }
+    public static  HashMap<Integer, List<Double>> secondOrderDiffUsingRk4(double xnow,double ynow,double znow,double h,TriFunction<Double> func,int itr){
+        //stores values  x,y,z  for each iteration including initial ones
+        HashMap<Integer,List<Double>> values=new LinkedHashMap<>(itr+1);
+
+        for(int i=0;i<=itr;i++){
+
+            //stores current value of x,y,z
+            values.put(i,List.of(xnow,ynow,znow));
+
+            //find k1,l1,k2,l2,k3,l3,k4,l4 for necessary computation
+           double kOne=h*znow;
+           double lOne=h* func.apply(xnow,ynow,znow);
+           double kTwo=h*(znow+lOne/2);
+           double lTwo=h* func.apply(xnow+h/2,ynow+kOne/2,znow+lOne/2);
+           double kThree=h* (znow+lTwo/2);
+           double lThree=h* func.apply(xnow+h/2,ynow+kTwo/2,znow+lTwo/2);
+           double kFour=h*(znow+lThree);
+           double lFour=h*func.apply(xnow+h,ynow+kThree,znow+lThree);
+
+
+         //follows rk4/classical method yn+1 and zn+1 formula
+           double ynext=ynow+(1.0/6)*(kOne+(2*kTwo)+(2*kThree)+kFour);
+           double znext=znow+(1.0/6)*(lOne+(2*lTwo)+(2*lThree)+lFour);
+
+           //update the values
+           ynow=ynext;
+           znow=znext;
+           xnow=xnow+h;
         }
         return values;
     }
